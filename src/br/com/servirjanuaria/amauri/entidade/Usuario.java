ca384fce264979e112a608cb0062ff5104a8cbd7
Amauri Pereira <amauri.pj@hotmail.com>
2015-03-06 18:55:40 -0300

    Classe usuario

---
+++ src/br/com/servirjanuaria/amauri/entidade/Usuario.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.servirjanuaria.amauri.entidade;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Amauri
+ */
+public class Usuario {
+    private int idlogin;
+    private String nome;
+    private String usuario;
+    private String senha;
+    private int nivelUsuario;
+    private Date dataCadastro;
+
+    public int getIdlogin() {
+        return idlogin;
+    }
+
+    public void setIdlogin(int idlogin) {
+        this.idlogin = idlogin;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNivelUsuario() {
+        return nivelUsuario;
+    }
+
+    public void setNivelUsuario(int nivelUsuario) {
+        this.nivelUsuario = nivelUsuario;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    
+    
+       
+    
+}
