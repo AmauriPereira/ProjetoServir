147bbe0cc0a4364bbf65ce4d688f83c7b46189ee
Amauri Pereira <amauri.pj@hotmail.com>
2015-05-24 14:18:07 -0300

    Criação UsuarioDAO1

---
+++ src/br/com/servirjanuaria/amauri/dataAccess/Usuario1DAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.servirjanuaria.amauri.dataAccess;
+
+import br.com.servirjanuaria.amauri.domainModel.Aluno;
+import br.com.servirjanuaria.amauri.domainModel.Usuario;
+import br.com.servirjanuaria.amauri.domainModel.repositorios.UsuarioRepositorio;
+import java.util.List;
+
+/**
+ *
+ * @author Amauri
+ */
+public class Usuario1DAO extends DaoGenerico<Usuario> implements UsuarioRepositorio {
+
+    public Usuario1DAO() {
+        super(Usuario.class);
+    }
+
+    @Override
+    public Usuario AbrirPorNomeDeUsuario(String nome) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Usuario> buscar(Usuario filtro) {
+        return Like("usuario", filtro.getUsuario())
+                .buscar();
+
+    }
+    
+    
+
+}
